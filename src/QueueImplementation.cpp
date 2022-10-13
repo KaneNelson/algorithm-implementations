bc5d04b5a84a1afa36092785029f6867c60233b8
Kane Nelson <94143226+KaneNelson@users.noreply.github.com>
2022-10-12 22:05:27 -0400

    upload

---
+++ src/QueueImplementation.cpp
@@ -0,0 +1,98 @@
+﻿#include <iostream>
+using namespace std;
+
+class Queue
+{
+private:
+	float* queueArray;
+	int queueSize;
+	int front;
+	int rear;
+	int numItems;
+public:
+	Queue(int);
+	void enqueue(float);
+	void dequeue();
+	bool isEmpty();
+	bool isFull();
+	void display();
+};
+
+int main()
+{
+	Queue q(10);
+
+	char c = ' ';
+	while (c != 'q') {
+		cout << "Enter 'a' to add number to queue, 'd' to dequeue, 's' to show queue contents, 'q' to quit: " << endl;
+		cin >> c;
+
+		switch (c) {
+
+		case 'q':
+			break;
+
+		case 'a':
+			float n;
+			cout << "Enter value(float) to add to queue: ";
+			cin >> n;
+			q.enqueue(n);
+			break;
+
+		case 's':
+			q.display();
+			break;
+
+		case 'd':
+			q.dequeue();
+			break;
+
+		default:
+			cout << "Invalid character" << endl << endl;
+		}
+	}
+
+	return 0;
+}
+
+Queue::Queue(int x) {
+	queueSize = x;
+	queueArray = new float[x];
+	numItems = 0;
+	front = -1;
+	rear = -1;
+}
+void Queue::enqueue(float x) {
+	if (isFull())
+		cout << "Queue is full" << endl << endl;
+	else {
+		cout << "Enqueueing " << x << endl << endl;
+		rear++;
+		numItems++;
+		queueArray[rear % queueSize] = x;
+
+	}
+}
+void Queue::dequeue() {
+	if (isEmpty())
+		cout << "Queue is empty" << endl << endl;
+	else {
+		front++;
+		cout << queueArray[front] << " is dequeued" << endl << endl;
+		numItems--;
+	}
+}
+bool Queue::isEmpty() {
+	return numItems <= 0;
+}
+bool Queue::isFull() {
+	return numItems >= queueSize;
+}
+void Queue::display() {
+	cout << "Displaying queue contents from front to back: " << endl;
+	for (int i = 1; i <= numItems; i++) {
+		cout << queueArray[(front + i) % queueSize];
+		cout << endl;
+	}
+	cout << endl;
+}
---
+++ src/StackImplementation.cpp
@@ -0,0 +1,96 @@
+﻿#include "iostream"
+using namespace std;
+
+class Stack
+{
+private:
+	float* stackArray;
+	int stackSize;
+	int top;
+
+public:
+	Stack(int);
+	void push(float);
+	void pop();
+	bool isFull();
+	bool isEmpty();
+	void display();
+};
+
+int main()
+{
+	Stack myStack(10);
+
+
+	char c = ' ';
+	while (c != 'q') {
+		cout << "Enter 'a' to add number to stack, 'p' to pop, 'd' to display stack, 'q' to quit: " << endl;
+		cin >> c;
+		
+		switch (c) {
+		
+		case 'q':
+			break;
+		
+		case 'a':
+			float n;
+			cout << "Enter value(float) to push to stack: ";
+			cin >> n;
+			myStack.push(n);
+			break;
+
+		case 'd':
+			myStack.display();
+			break;
+
+		case 'p':
+			myStack.pop();
+			break;
+
+		default:
+			cout << "Invalid character" << endl << endl;;
+		}
+	}
+
+
+	return 0;
+}
+
+Stack::Stack(int x) {
+	stackArray = new float[x];
+	stackSize = x;
+	top = -1;
+}
+void Stack::push(float x) {
+	if (isFull()) {
+		cout << "The stack is full" << endl << endl;
+	}
+	else {
+		top++;
+		cout << "Pushing " << x << endl << endl;
+		stackArray[top] = x;
+
+	}
+
+}
+void Stack::pop() {
+	if (isEmpty()) {
+		cout << "The stack is empty\n";
+	}
+	else {
+		cout << "Popping " << stackArray[top] << endl << endl;
+		top--;
+	}
+}
+bool Stack::isFull() {
+	return top >= stackSize - 1;
+}
+bool Stack::isEmpty() {
+	return top <= -1;
+}
+void Stack::display() {
+	cout << "Displaying stack contents from top to bottom: " << endl;
+	for (int i = top; i >= 0; i--)
+		cout << stackArray[i] << " ";
+	cout << endl << endl;
+}
